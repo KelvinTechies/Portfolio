10efec4d168388c65025c557ef2ba53c84c5bdac
kelvinTechies <iamkelvincole@gmail.com>
2026-04-08 15:48:26 +0100

    done

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -110,7 +110,7 @@ const Portfolio = () => {
                 <li>
                   <figure>
                     <figcaption>
-                      <h3>MySounds Global</h3>
+                      <h3>MySoundsGlobal</h3>
                       <div className="row open-sans-font">
                         <div className="col-12 col-sm-6 mb-2">
                           <i className="fa fa-file-text-o pr-2"></i><span className="project-label">Project</span>: <span className="ft-wt-600 uppercase">Website</span>
